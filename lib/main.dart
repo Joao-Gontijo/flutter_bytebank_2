0fb8945e67beff8ac8cf8b8838ec26f815f0eb3e
Joao-Gontijo <jvgontijo166@gmail.com>
2022-02-20 21:05:29 -0300

    widgets alignment

--- lib/main.dart
+++ lib/main.dart
@@ -13,23 +13,44 @@ class ByteBankApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.green[900],
           title: const Text('Dashboard'),
         ),
-        body: Column(
-          children: [
-            Image.asset('images/bytebank_logo.png'),
-            Container(
-              height: 100,
-              width: 150,
-              color: Colors.green[900],
-              child: Column(
-                children: const [
-                  Icon(Icons.people),
-                  Text('Contacts'),
-                ],
-              ),
-            )
-          ],
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset('images/bytebank_logo.png'),
+              Container(
+                height: 100,
+                width: 150,
+                color: Colors.green[900],
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Icon(
+                        Icons.people,
+                        color: Colors.white,
+                        size: 24.0,
+                      ),
+                      Text(
+                        'Contacts',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
