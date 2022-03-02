f99b2ba8bee65d98eb33827849bd5b63bf25fe5f
Joao-Gontijo <jvgontijo166@gmail.com>
2022-03-02 15:47:01 -0300

    http interceptor

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,5 +1,30 @@
 import 'package:bytebank_2/http/key_consts.dart';
+import 'package:http/http.dart';
+import 'package:http_interceptor/http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    print('REQUEST');
+    print('url: ${data.url}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    print('RESPONSE');
+    print('status code: ${data.statusCode}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+}
 
 void findAll() async {
-  print(Constants.stringTeste);
+  final Client client =
+      InterceptedClient.build(interceptors: [LoggingInterceptor()]);
+  final Response response = await client.get(Constants.uri);
 }
