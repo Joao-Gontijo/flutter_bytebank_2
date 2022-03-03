2bb4dbfec9e427031e0c51441c5190438c340b4e
Joao-Gontijo <jvgontijo166@gmail.com>
2022-03-03 16:53:59 -0300

    showing transactions list on screen

--- lib/screens/transactions_list.dart
+++ lib/screens/transactions_list.dart
@@ -1,9 +1,9 @@
+import 'package:bytebank_2/components/progress.dart';
+import 'package:bytebank_2/http/webclient.dart';
 import 'package:bytebank_2/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 class TransactionsList extends StatelessWidget {
-  final List<Transaction> transactions = [];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,29 +11,51 @@ class TransactionsList extends StatelessWidget {
         title: const Text('Transactions'),
         backgroundColor: Theme.of(context).primaryColor,
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final Transaction transaction = transactions[index];
-          return Card(
-            child: ListTile(
-              leading: const Icon(Icons.monetization_on),
-              title: Text(
-                transaction.value.toString(),
-                style: const TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: Text(
-                transaction.contact.accountNumber.toString(),
-                style: const TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          );
+      body: FutureBuilder(
+        future: Future.delayed(const Duration(seconds: 1), () => findAll()),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Progress();
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Transaction>? transactions =
+                  snapshot.data as List<Transaction>?;
+              if (transactions!.isEmpty) {
+                return const Center(child: Text('No transactions found!'));
+              } else {
+                return ListView.builder(
+                  itemBuilder: (context, index) {
+                    final Transaction transaction = transactions[index];
+                    return Card(
+                      child: ListTile(
+                        leading: const Icon(Icons.monetization_on),
+                        title: Text(
+                          transaction.value.toString(),
+                          style: const TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        subtitle: Text(
+                          transaction.contact.accountNumber.toString(),
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: transactions.length,
+                );
+              }
+          }
+          return const Text('Unknown Error');
         },
-        itemCount: transactions.length,
       ),
     );
   }
